4630f7ebcb34fd232764e5dd218bb945421d6d9c
Andrew Jones <andrewjones232@gmail.com>
2017-07-15 23:17:06 -0400

    cleaned up main.js and read in arguments

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 var backup = require('./backupGenerator.js');
 var logger = require('./logger.js');
 
-function generateBackupsIfThreeAM() {
-	console.log('checking date');
-	let d = new Date();
-	let h = d.getHours();
-	console.log(h);
-	console.log(d);
+var args = process.argv.slice(2);
 
-	// note: the docker daemon reports a time 4 hours ahead of EST, 
-	// which is why this number is 7
-	// todo: more elegantly read in current time from docker.
-	if (h === 3) {
-		console.log('updating')
-		try {
-			backup.updateBackups();
-		}
-		catch(e) {
-			logger.errorLog('error updating backups ' + e.message + '\n' + e.stack);
-		}
-		
+function generateBackups() {
+
+
+	console.log('attempting backup generation...')
+	try {
+		backup.updateBackups();
 	}
+	catch(e) {
+		logger.errorLog('error updating backups ' + e.message + '\n' + e.stack);
+	}
+		
 }
 
-generateBackupsIfThreeAM();
 
-//check once per hour
 setInterval(generateBackupsIfThreeAM, 1000 * 60 * 60);
