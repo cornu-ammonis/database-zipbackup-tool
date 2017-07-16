6a2ce7d17f4ef2b9e7b9fbe92bc43aa6e9b9de61
Andrew Jones <andrewjones232@gmail.com>
2017-07-15 23:41:42 -0400

    setTimeOut

--- main.js
+++ main.js
@@ -79,3 +79,8 @@ else if (args.length > 1) {
 		hoursInterval = parseInt(args[1]);
 	}
 }
+
+console.log('starting setTimeout with ' + hoursDelay + 
+	' hours of delay and a ' + hoursInterval + ' hour interval between successive backups');
+
+setTimeout(firstBackupAndStartInterval.bind(hoursInterval), hoursDelay); 
