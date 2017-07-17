21842314b3f54c86b55575b44350ec7d8f6fb5b3
Andrew Jones <andrewjones232@gmail.com>
2017-07-16 23:03:45 -0400

    error logging for main loop

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ DOCKER USAGE:
 */
 
 
-var backup = require('./backupGenerator.js');
-var logger = require('./logger.js');
+const backup = require('./backupGenerator.js');
+const logger = require('./logger.js');
 
 function generateBackups() {
 
@@ -35,57 +35,78 @@ function generateBackups() {
 
 // runs backup, then starts setInterval with given hoursInterval delay
 function firstBackupAndStartInterval(_hoursInterval) {
-	generateBackups();
+	try {
+		generateBackups();
 
-	setInterval(generateBackups, _hoursInterval * 60 * 60 * 1000)
-}
-
-
-// remove 'node' and 'main.js' from arguments list so that needed arguments
-// will start at [0]
-var args = process.argv.slice(2);
-
-// defaults
-var hoursDelay = 0.01667; // equivalent to ~60 seconds, gives user chance to abort
-var hoursInterval = 24;
-
-// case where user provides no arguments
-if (args.length == 0) {
-	
-	//defaults
-	console.log('defaulting to immediate backup and 24 hour hoursInterval');
-}
-
-// case where user only provides delay argument
-else if (args.length == 1) {
-	if (isNaN(args[0])) {
-		console.log('you didnt enter a number for the hoursDelay, defaulting to generate backups after 10 seconds');
+		setInterval(generateBackups, _hoursInterval * 60 * 60 * 1000)
 	}
-	else {
-		hoursDelay = parseInt(args[0]);
+	catch(e) {
+		let error = 'error in firstBackupAndStartInterval' + '\n' + e.message + '\n' + e.stack;
+		console.log(error);
+		logger.errorLog(error);
 	}
 
 }
 
-// case where user provides delay and hoursinterval argument
-else if (args.length > 1) {
-	
-	if (isNaN(args[0])) {
-		console.log('you didnt enter a number for the hoursDelay, defaulting to generate backups after 10 seconds');
-	}
-	else {
-		hoursDelay = parseInt(args[0]);
+try {
+
+
+	// remove 'node' and 'main.js' from arguments list so that needed arguments
+	// will start at [0]
+	var args = process.argv.slice(2);
+
+	// defaults
+	var hoursDelay = 0.01667; // equivalent to ~60 seconds, gives user chance to abort
+	var hoursInterval = 24;
+
+	// case where user provides no arguments
+	if (args.length == 0) {
+		
+		//defaults
+		console.log('defaulting to immediate backup and 24 hour hoursInterval');
 	}
 
-	if (isNaN(args[1])) {
-		console.log('you didnt enter a number for hoursInterval, defaulting to 24');
+	// case where user only provides delay argument
+	else if (args.length == 1) {
+		if (isNaN(args[0])) {
+			console.log('you didnt enter a number for the hoursDelay, defaulting to generate backups after 10 seconds');
+		}
+		else {
+			hoursDelay = parseInt(args[0]);
+		}
+
 	}
-	else {
-		hoursInterval = parseInt(args[1]);
+
+	// case where user provides delay and hoursinterval argument
+	else if (args.length > 1) {
+		
+		if (isNaN(args[0])) {
+			console.log('you didnt enter a number for the hoursDelay, defaulting to generate backups after 10 seconds');
+		}
+		else {
+			hoursDelay = parseInt(args[0]);
+		}
+
+		if (isNaN(args[1])) {
+			console.log('you didnt enter a number for hoursInterval, defaulting to 24');
+		}
+		else {
+			hoursInterval = parseInt(args[1]);
+		}
 	}
+
+	console.log('starting setTimeout with ' + hoursDelay + 
+		' hours of delay and a ' + hoursInterval + ' hour interval between successive backups');
+
+	setTimeout( firstBackupAndStartInterval.bind(null, hoursInterval) , hoursDelay * 60 * 60 * 1000); 
+
 }
 
-console.log('starting setTimeout with ' + hoursDelay + 
-	' hours of delay and a ' + hoursInterval + ' hour interval between successive backups');
+catch(e) {
 
-setTimeout( firstBackupAndStartInterval.bind(null, hoursInterval) , hoursDelay * 60 * 60 * 1000); 
+	let error = 'error starting or error within main event loop. attempting to write to error log' + 
+		e.message + '\n' + e.stack;
+	console.log(error);
+
+	logger.errorLog(error);
+}
